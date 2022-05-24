8f86d10ba6d51d549a4ab65c848a189731b77c49
binhthanh <thanhbinhnguyen097@gmail.com>
2022-05-25 00:36:02 +0700

    add search-bar and login

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { BsSearch, BsCart } from "react-icons/bs";
+import { BsSearch, BsCart, BsFillPersonFill } from "react-icons/bs";
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import logo from '../../logo.svg'
 import { Link } from 'react-router-dom';
+import './Header.scss'
 
 const Header = () => {
     return (
@@ -12,26 +13,31 @@ const Header = () => {
                     <img className="opacity-100" src={logo} 
                     width="30"
                     height="40"
-                    alt="React Bootstrap logo"/>
+                    alt="logo"/>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll"/>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#Iphone">Iphone</Nav.Link>
-                        <Nav.Link href="#Ipad">Ipad</Nav.Link>
-                        <Nav.Link href="#MacBook">MacBook</Nav.Link>
-                        <Nav.Link href="#Watch">Apple Watch</Nav.Link>
-                        <Nav.Link href="#phukien">Phụ kiện</Nav.Link>
-                    </Nav>  
-                    <Nav>
-                        <Nav.Link className="text-white" href="#search">
-                            <BsSearch fontSize='20px' bg='light'/>
-                        </Nav.Link>
-                        <Nav.Link className="text-white" as={Link} to="/cart" >
-                            <BsCart fontSize='20px'/>
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+                <form className="search-bar">
+                    <input type="search" placeholder="Tìm kiếm"></input>
+                    <button type="submit"><BsSearch fontSize='20px'/></button>
+                </form>
+                <Navbar.Toggle/>
+                    <Navbar.Collapse>
+                        <Nav className="me-auto">
+                            <Nav.Link href="#Iphone">Iphone</Nav.Link>
+                            <Nav.Link href="#Ipad">Ipad</Nav.Link>
+                            <Nav.Link href="#MacBook">MacBook</Nav.Link>
+                            <Nav.Link href="#Watch">Apple Watch</Nav.Link>
+                            <Nav.Link href="#phukien">Phụ kiện</Nav.Link>
+                        </Nav>  
+                        <Nav>
+                            <Nav.Link className="text-white" as={Link} to="/cart" >
+                                <BsCart fontSize='20px'/>
+                            </Nav.Link>
+                            <Nav.Link className="text-white" as={Link} to="/login" >
+                                <BsFillPersonFill fontSize='20px'/>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                
             </Container>
         </Navbar >
     )
