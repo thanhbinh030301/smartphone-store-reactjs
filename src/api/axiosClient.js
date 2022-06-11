07a96a871d3eb23a8c51bc5446fd0a24ab5cb720
binhthanh <thanhbinhnguyen097@gmail.com>
2022-06-11 17:28:06 +0700

    update path api

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:5000/',
+    baseURL: 'https://phone-store-web.herokuapp.com/',
     headers: {
         'content-type': 'application/json',
     }
